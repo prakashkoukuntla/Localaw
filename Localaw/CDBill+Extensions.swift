d7f15c1fecc3c92ab9992dcdaa51a830be0aa62f
Prakash Koukuntla <pkoukuntla123@gmail.com>
2021-11-10 18:00:56 -0700

    added bill category with relationships in core data

--- Localaw/CDBill+Extensions.swift
+++ Localaw/CDBill+Extensions.swift
@@ -5,5 +5,26 @@
 import Foundation
 
 extension CDBill: Bill {
+    var subjects: [BillCategory] {
+        categories as? Set<CDBillCategory>
+    }
+    
+    var sponsors: [Legislator] {
+        <#code#>
+    }
+    
+    var committees: [Committee] {
+        <#code#>
+    }
+    
+    var name: String { cdName ?? "" }
+    
+    var longSummary: String { cdLongSummary ?? "" }
+    
+    var shortSummary: String { cdShortSummary ?? "" }
+    
+    var session: String { cdSession ?? "" }
+    
+    var billID: String { cdBillID ?? "" }
     
 }
