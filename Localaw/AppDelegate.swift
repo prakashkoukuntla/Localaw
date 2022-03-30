b906cf2366051a0b2a635be09aad42be5c707162
Prakash Koukuntla <pkoukuntla123@gmail.com>
2022-03-30 17:13:26 -0600

    insignificant changes

--- Localaw/AppDelegate.swift
+++ Localaw/AppDelegate.swift
@@ -10,9 +10,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let database = Database()
     let webservice = WebService()
-
+    //lazy var timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(timerUpdated(_:)), userInfo: nil, repeats: true)
+    lazy var timer = Timer(timeInterval: 10, target: self, selector: #selector(timerUpdated(_:)), userInfo: nil, repeats: true)
+    var backgroundCompletionHandler: (() -> Void)?
+    private lazy var urlSession: URLSession = {
+        let config = URLSessionConfiguration.background(withIdentifier: "MySession")
+        config.isDiscretionary = true
+        config.sessionSendsLaunchEvents = true
+        return URLSession(configuration: config, delegate: self, delegateQueue: nil)
+    }()
+    
+    @objc func timerUpdated(_ timer: Timer) {
+        let url = URL(string: WebService.Constants.billsURL)!
+        let backgroundTask = urlSession.downloadTask(with: url)
+        backgroundTask.earliestBeginDate = Date()
+        backgroundTask.countOfBytesClientExpectsToSend = 200
+        backgroundTask.countOfBytesClientExpectsToReceive = 500 * 1024
+        backgroundTask.resume()
+        print("timer timed")
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        timer.fire()
         webservice.fetchBills { [self] result in
             database.context.perform {
                 switch result {
@@ -68,8 +88,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication,
+                     handleEventsForBackgroundURLSession identifier: String,
+                     completionHandler: @escaping () -> Void) {
+            backgroundCompletionHandler = completionHandler
+    }
+    
 }
 
 extension Notification.Name {
     static let billsUpdated = Notification.Name(rawValue: "Bills Updated")
+}
+
+extension AppDelegate: URLSessionDelegate {
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        DispatchQueue.main.async {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let backgroundCompletionHandler =
+                appDelegate.backgroundCompletionHandler else {
+                    return
+            }
+            backgroundCompletionHandler()
+        }
+    }
 }
